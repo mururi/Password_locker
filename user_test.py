2043adbdd6619eb95bc63723b1b418b413066c51
Dennis Kiboi <mururi.kiboi@gmail.com>
2022-02-20 10:32:09 +0300

    Add setUp method

--- user_test.py
+++ user_test.py
@@ -8,6 +8,12 @@ class TestUser(unittest.TestCase):
         unittest.TestCase: TestCase class that helps in creating test cases
     """
 
+    def setUp(self):
+        """
+        setUp method to run before each test case. Creates a user object.
+        """
+
+        self.new_user = User("Dennis Kiboi", "myPassword")
 
 if __name__ == '__main__':
     unittest.main()
