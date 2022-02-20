aeceb7819a64f1e408d4faf36d800046d89a2d3f
Dennis Kiboi <mururi.kiboi@gmail.com>
2022-02-20 10:27:56 +0300

    Set up user_test class

---
+++ user_test.py
@@ -0,0 +1,13 @@
+import unittest
+from user import User
+
+class TestUser(unittest.TestCase):
+    """
+    Test class that defines cases for the user class behaviors.
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    """
+
+
+if __name__ == '__main__':
+    unittest.main()
