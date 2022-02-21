63569af9bacb1e17946ef15e50d4e39bfd915d8d
Dennis Kiboi <mururi.kiboi@gmail.com>
2022-02-21 16:15:41 +0300

    Add save_user function

--- run.py
+++ run.py
@@ -1,1 +1,19 @@
-#!/usr/bin/env python3.9
+#!/usr/bin/env python3.8
+from user import User
+from credentials import Credential
+
+def create_user(login, login_pass):
+    """
+    Function to create a new user
+    """
+
+    new_user = User(login, login_pass)
+    return new_user
+
+def save_user(user):
+    """
+    Function that saves user details to users_list
+    """
+
+    user.save_user()
+
